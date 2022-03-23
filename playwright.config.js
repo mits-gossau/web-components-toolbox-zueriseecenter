b933b468df7ba6ec03376d90348ce39379cbb6b3
edmgb <evangelos.dinopoulos@mgb.ch>
2022-03-23 08:04:34 +0100

    working

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ const { devices } = require('@playwright/test');
 const config = {
     //forbidOnly: !!process.env.CI,
     //retries: process.env.CI ? 2 : 0,
-    timeout: 30000,
+    //timeout: 30000,
     use: {
         baseURL: 'http://localhost:4200',
         trace: 'on-first-retry',
