98b601a3d93e5add98f1d18ec5ad849bc197434d
edmgb <evangelos.dinopoulos@mgb.ch>
2022-03-30 10:20:56 +0200

    work

--- playwright.config.js
+++ playwright.config.js
@@ -41,6 +41,7 @@ const config = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: true
   },
 
   /* Configure projects for major browsers */
